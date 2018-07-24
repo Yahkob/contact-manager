f1f6c6a6a60db5cb0cb313a3391ba2c9b475ef61
Jason Bland <jtbland@gmail.com>
2018-07-24 07:19:31 -0700

    update index.js

--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import {combineReducers} from 'redux';
-import {reducer as formReducer} from 'redux-form';
-import {contactsReducer} from './contactsReducer';
-
-export default combineReducers({contacts: contactsReducer, form: formReducer});
